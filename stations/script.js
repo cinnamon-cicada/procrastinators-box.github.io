e86ba5826602abd1e8437076869f707c5b2915cb
procrastinators-box <86079782+procrastinators-box@users.noreply.github.com>
2021-08-22 15:10:15 +0000

    Update script.js

--- stations/script.js
+++ stations/script.js
@@ -2,3 +2,13 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
+
+var primaryTheme = document.getElementsByName('primary_theme');
+
+for (var i = 0, length = primaryTheme.length; i < length; i++) {
+  if (primaryTheme[i].checked) {
+    if (primaryTheme[i].value ==
+    
+    break;
+  }
+}
