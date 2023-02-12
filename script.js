2d67aff2526848d1af52b68bc45297fdfbb15183
procrastinators-box <86079782+procrastinators-box@users.noreply.github.com>
2023-02-12 03:14:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ var stationName = document.getElementsByName('addTitle');
 var seconds = 0, minutes = 0, hours = 0, stopwatchOn = false;
 var stations = [];
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9OdbrBcnMpmSjWQA8qMXfu79319ILVd0",
+  authDomain: "procrastinator-s-box.firebaseapp.com",
+  projectId: "procrastinator-s-box",
+  storageBucket: "procrastinator-s-box.appspot.com",
+  messagingSenderId: "1091604736507",
+  appId: "1:1091604736507:web:29ec163b373ecfeeb7435e",
+  measurementId: "G-3J2SF14LVB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function makeStation() {
   for (var i = 0, length = primaryTheme.length; i < length; i++) {
     if(!(stationName === "")) {
