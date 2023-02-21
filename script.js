d2a88d9056a21baf1cbae7f54a58258066c78f57
procrastinators-box <86079782+procrastinators-box@users.noreply.github.com>
2023-02-21 02:50:33 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB9OdbrBcnMpmSjWQA8qMXfu79319ILVd0",
-  authDomain: "procrastinator-s-box.firebaseapp.com",
-  projectId: "procrastinator-s-box",
-  storageBucket: "procrastinator-s-box.appspot.com",
-  messagingSenderId: "1091604736507",
-  appId: "1:1091604736507:web:29ec163b373ecfeeb7435e",
-  measurementId: "G-3J2SF14LVB"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'module';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-var primaryTheme = document.getElementsByName('primary_theme');
-var stationName = document.getElementsByName('addTitle');
-
-var seconds = 0, minutes = 0, hours = 0, stopwatchOn = false;
-var stations = [];
-
-function testFB() {
-  //console.log(firebase);
-}
-
 function makeStation() {
   for (var i = 0, length = primaryTheme.length; i < length; i++) {
     if(!(stationName === "")) {
