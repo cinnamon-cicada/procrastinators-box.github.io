068623b722579301268df4cb4958783d0b307972
procrastinators-box <86079782+procrastinators-box@users.noreply.github.com>
2023-02-21 02:52:04 +0000

    Update fbmodule.js

--- fbmodule.js
+++ fbmodule.js
@@ -1,1 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9OdbrBcnMpmSjWQA8qMXfu79319ILVd0",
+  authDomain: "procrastinator-s-box.firebaseapp.com",
+  projectId: "procrastinator-s-box",
+  storageBucket: "procrastinator-s-box.appspot.com",
+  messagingSenderId: "1091604736507",
+  appId: "1:1091604736507:web:29ec163b373ecfeeb7435e",
+  measurementId: "G-3J2SF14LVB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+/*
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'module';
+document.getElementsByTagName('head')[0].appendChild(script);
+*/
+
+function testFB() {
+  //console.log(firebase);
+}
