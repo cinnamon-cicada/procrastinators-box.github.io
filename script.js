8f78d9d803c004a169879f922aec6a65129cde21
procrastinators-box <86079782+procrastinators-box@users.noreply.github.com>
2023-03-11 02:20:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -138,10 +138,48 @@ function loadAvatar() {
   
 }
 
-const cookieValue = document.cookie
-  .split("; ")
-  .find((row) => row.startsWith("test2="))
-  ?.split("=")[1];
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+if (storageAvailable('localStorage')) {
+  if (!localStorage.getItem('customStations')) {
+    addCustomizedStations();
+  } else {
+    setHtml();
+  }
+} else {
+}
+
+addCustomizedStations(input) {
+  
+}
+
+setHtml() {
+  
+}
+
 
   //jump to place: document.getElementById("jump_to_this_location").scrollIntoView({behavior: 'smooth'});
   //add a SUBMIT button to know when to collect information
